89da2fd6b2bb89b20be5f6ddce0d29b4d4a0364b
Adolfo Gustavo Velazquez Luna <adolfo.velazquez@coppel.com>
2024-03-04 17:41:59 -0700

    se agrega la url de la vista listoffer

--- Proyecto_Marketplace/apps/cargas_mkp/urls.py
+++ Proyecto_Marketplace/apps/cargas_mkp/urls.py
@@ -1,3 +1,4 @@
+from typing import ValuesView
 from django.contrib import admin
 from django.urls import path
 from . import views
@@ -6,4 +7,5 @@ app_name = "carga_mkp"
 urlpatterns = [
     #path('admin/', admin.site.urls),
     path('home',views.HomeView.as_view(), name='inicio'),
+    path('listviewOfferSeller/',views.OfertasSellerCount.as_view(), name='listOfferSeller'),
 ]
