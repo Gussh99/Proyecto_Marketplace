c458e5b5f08231a8626206b8be6b61d4bcbb26da
Adolfo Gustavo Velazquez Luna <adolfo.velazquez@coppel.com>
2024-03-04 17:44:27 -0700

    se apunto como se usa las bases de datos en las apps

--- Proyecto_Marketplace/Proyecto_Marketplace/settings/local.py
+++ Proyecto_Marketplace/Proyecto_Marketplace/settings/local.py
@@ -1,6 +1,6 @@
 from .base import *
 import ibm_db_django
-
+#from .database_router import AuthRouter
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -12,15 +12,26 @@ ALLOWED_HOSTS = ['*']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME':'market',
+        'USER'     : 'Gustavo',
+        'PASSWORD' : 'Gustavo',
+        'HOST'     : 'localhost',
+        'PORT'     : '5432',
+        #'PCONNECT' :  True, 
+    },
+    'DB_TiendaVirtual':{
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME':'tiendavirtual',
         'USER'     : '90308578',
         'PASSWORD' : 'AF4E0E621EEEA652A43A6DBDA92A7242666A575C7AD0EAB803D4A9678FEDD393',
         'HOST'     : '10.44.1.159',
         'PORT'     : '5432',
-        #'PCONNECT' :  True, 
     },
     'DB2_Authoring':{
         'ENGINE': 'ibm_db_django',
+        """'OPTIONS':{
+            'options':-c search_path=DB2_Authoring,WCSSYS
+        },"""
         'NAME':'coppeldb',
         'USER'     : 'ecomm',
         'PASSWORD' : 'Cop1Db2',
@@ -29,6 +40,11 @@ DATABASES = {
         'PCONNECT' :  True, 
     }
 }
+#clases personalisa para las DB
+#en un futuro se usara
+DATABASE_ROUTERS = (
+    'apps.cargas_mkp.database_router.AuthRouterModlelDB',
+) #['path.to.AuthRouter']
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
