b6edf3396dfd9ecc0d87f29ae307bf3ed7b0ef1f
Adolfo Gustavo Velazquez Luna <adolfo.velazquez@coppel.com>
2024-03-04 17:41:10 -0700

    se agrega la nueva vista de listview de las offert

--- Proyecto_Marketplace/apps/cargas_mkp/views.py
+++ Proyecto_Marketplace/apps/cargas_mkp/views.py
@@ -1,10 +1,13 @@
 import datetime
+from typing import Any
+from django.db.models import Q
 from django.shortcuts import render
+from .forms import VistaOfertasSeller
 from django.views.generic import (
     TemplateView,
+    ListView
 )
-
-
+from .models import *
 # Create your views here.
 class FechaMixin(object):
      
@@ -12,6 +15,21 @@ class FechaMixin(object):
          context = super(FechaMixin, self).get_context_data(**kwargs)
          context['fecha'] = datetime.datetime.now()
          return context
+    
+class OfertasSellerCount(ListView):
+    model:CatOfertasmarketplace
+    template_name = "Ofertas_por_seller.html"
+    form_class = VistaOfertasSeller
+    context_object_name = 'catoffer' 
+
+    def get_queryset(self):
+        palabra_clave = self.request.GET.get("kword", '')
+        lista = CatOfertasmarketplace.objects.filter(
+            Q(nom_parentpartnumber = palabra_clave) | Q( nom_tienda= palabra_clave) #| Q(idu_tienda = palabra_clave)           
+        )
+        print(lista)
+        return lista 
+
 
 
 class HomeView(FechaMixin, TemplateView):
